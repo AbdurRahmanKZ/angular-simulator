4859e6dd4c794d1bff122ee5c8da12c3d061496e
AbdurRahmanKZ <titantito2002@gmail.com>
2026-05-23 00:45:34 +0500

    feat: layout and generic classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import './training';
+import { Color } from '../enums/color';
+import './collection';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,31 @@ import './training';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
+
 export class AppComponent {
+  titleName: string = 'румтибет';
+
+  checlColor(color: string):boolean {
+    if (color === Color.Red) return true
+    if (color === Color.Green) return true
+    if (color === Color.Blue) return true
+    return false
+  }
+
+  constructor() {
+    this.saveLastLoad();
+    this.saveQtyLoad();
+    console.log(localStorage.getItem('lastLoadDate'))
+    console.log(localStorage.getItem('qtyLoad'))
+  }
+
+  saveLastLoad():void {
+    const time = new Date().toString();
+    localStorage.setItem('lastLoadDate', time);
+  }
+
+  saveQtyLoad():void {
+    const currentQty = Number(localStorage.getItem('qtyLoad') || 0);
+    localStorage.setItem('qtyLoad', String(currentQty + 1));
+  }
 }
