c8014ee08880a5f68ef987f7f0bf50700eb2a218
AbdurRahmanKZ <titantito2002@gmail.com>
2026-06-04 15:39:22 +0500

    add comlete version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JsonPipe, DatePipe } from '@angular/common';
 import { Color } from '../enums/color';
 import './training';
 import './collection';
@@ -8,17 +9,27 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 
 export class AppComponent {
-  titleName: string = 'румтибет';
+  public titleName: string = 'румтибет';
+  public isLoading: boolean = true;
+  public tour = {
+    location: '',
+    date: '',
+    participants: ''
+  }
+  public interactive: 'counter' | 'date' = 'counter';
+  public count: number = 0;
+  public currentDate = new Date();
+  private dateTimerId?: ReturnType<typeof setInterval>
+  public liveInput: string = '';
 
-  isLoading: boolean = true;
 
-  facilities: facility[] = [
+  public facilities: facility[] = [
     {
       id: 1,
       img: '/pictures/people.png',
@@ -26,20 +37,19 @@ export class AppComponent {
       desc: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
     },
     {
-    id: 2,
-    img: '/pictures/shield.png',
-    title: 'Безопасный поход',
-    desc: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
+      id: 2,
+      img: '/pictures/shield.png',
+      title: 'Безопасный поход',
+      desc: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
     },
     {
-    id: 3,
-    img: '/pictures/birk.png',
-    title: 'Лояльные цены',
-    desc: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
+      id: 3,
+      img: '/pictures/birk.png',
+      title: 'Лояльные цены',
+      desc: 'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.'
     }
   ]
 
-  inputs: string = ''
 
   constructor() {
     this.saveLastLoad();
@@ -48,28 +58,28 @@ export class AppComponent {
     console.log(localStorage.getItem('qtyLoad'))
   }
 
-  checkColor(color: string): boolean {
+  private checkColor(color: string): boolean {
     if (color === Color.Red) return true
     if (color === Color.Green) return true
     if (color === Color.Blue) return true
     return false
   }
 
-  saveLastLoad(): void {
+  private saveLastLoad(): void {
     const time = new Date().toString();
     localStorage.setItem('lastLoadDate', time);
   }
 
-  saveQtyLoad(): void {
+  private saveQtyLoad(): void {
     const currentQty = Number(localStorage.getItem('qtyLoad') || 0);
     localStorage.setItem('qtyLoad', String(currentQty + 1));
   }
 
-  ngOnInit(): void {
-    setTimeout(() => {this.isLoading = false}, 2000);
+  private ngOnInit(): void {
+    setTimeout(() => { this.isLoading = false }, 2000);
   }
 
-  changeColor(event: Event): void {
+  public changeColor(event: Event): void {
     const element = event.currentTarget as HTMLElement;
     element.style.filter = 'invert(1)';
     element.style.position = 'invert(1)';
@@ -80,8 +90,27 @@ export class AppComponent {
     element.style.transition = 'transform 0.3s ease';
   }
 
-  regenColor(event: Event): void {
+  public regenColor(event: Event): void {
     const element = event.currentTarget as HTMLElement;
     element.style = '';
+  }
+
+  public get isSearchDisabled(): boolean {
+    return !this.tour.date || !this.tour.location || !this.tour.participants
+  }
+
+  public showDate(): void {
+    this.currentDate = new Date();
+    this.interactive = 'date';
+    clearInterval(this.dateTimerId);
+
+    this.dateTimerId = setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000)
+  }
+
+  public showCounter(): void {
+    clearInterval(this.dateTimerId);
+    this.interactive = 'counter';
   }
 }
