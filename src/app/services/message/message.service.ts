9809ea619063e086d3707a3bb44b8eba69e777f4
AbdurRahmanKZ <titantito2002@gmail.com>
2026-06-26 11:57:44 +0500

    refactor: message service as behavior subject & observable

--- src/app/services/message/message.service.ts
+++ src/app/services/message/message.service.ts
@@ -1,31 +1,43 @@
 import { Injectable } from '@angular/core';
 import { messageTypes, IMessage } from './message.type';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
   readonly messageTypes = messageTypes;
-  private messages: IMessage[] = [];
   private nextId: number = 1;
 
-  get list(): readonly IMessage[] {
-    return this.messages;
-  }
+  // private messages: IMessage[] = [];
+  // get list(): readonly IMessage[] {
+  //   return this.messages;
+  // }
+
+  private messagesSubject = new BehaviorSubject<IMessage[]>([]);
+  readonly messages$ = this.messagesSubject.asObservable();
 
   private addMessage(newMessage: Omit<IMessage, 'id'>): void {
-    const message = {
-      id: ++this.nextId,
+    const message: IMessage = {
+      id: this.nextId++,
       ...newMessage
-    }
-    this.messages.unshift(message);
+    };
+    // this.messages.unshift(message);
+
+    this.messagesSubject.next([
+      message,
+      ...this.messagesSubject.value
+    ])
     setTimeout(() => {
       this.closeMessage(message.id);
     }, 5000);
   }
 
   closeMessage(id: number): void {
-    this.messages = this.messages.filter(message => message.id != id);
+    // this.messages = this.messages.filter(message => message.id != id);
+    this.messagesSubject.next(
+      this.messagesSubject.value.filter(message => message.id != id)
+    )
   }
 
   showWarn(message: string): void {
