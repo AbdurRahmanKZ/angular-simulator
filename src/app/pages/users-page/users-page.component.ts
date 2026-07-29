0e49756de4ee8972e6cf7a72627814b36d07880c
AbdurRahmanKZ <titantito2002@gmail.com>
2026-07-29 13:34:39 +0500

    add users filter component

--- src/app/pages/users-page/users-page.component.ts
+++ src/app/pages/users-page/users-page.component.ts
@@ -2,19 +2,44 @@ import { Component, inject } from '@angular/core';
 import { UserService } from '../../services/user/user.service';
 import { IUsers } from '../../interfaces/users/users';
 import { AsyncPipe } from '@angular/common';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
 import { UserCardComponent } from './user-card/user-card.component';
 import { UserCreateComponent } from './user-create/user-create.component';
+import { UsersFilterComponent } from './users-filter/users-filter.component';
 @Component({
   selector: 'app-users-page',
-  imports: [AsyncPipe, UserCardComponent, UserCreateComponent],
+  imports: [AsyncPipe, UserCardComponent, UserCreateComponent, UsersFilterComponent],
   templateUrl: './users-page.component.html',
   styleUrl: './users-page.component.scss',
 })
 export class UsersPageComponent {
   public userService: UserService = inject(UserService);
+  private readonly filterSubject = new BehaviorSubject<string>('');
 
-  public users$: Observable<IUsers[]> = this.userService.getUsers();
+  public readonly users$: Observable<IUsers[]> = this.userService.getUsers();
+
+  public readonly filteredUsers$: Observable<IUsers[]> =
+    combineLatest([
+      this.users$,
+      this.filterSubject,
+    ]).pipe(
+      map(([users, filterValue]: [IUsers[], string]) => {
+        if (!filterValue) {
+          return users;
+        }
+
+        return users.filter((user: IUsers) =>
+          user.name
+            .trim()
+            .toLowerCase()
+            .includes(filterValue)
+        );
+      })
+    );
+
+  public onFilterChanged(value: string): void {
+    this.filterSubject.next(value);
+  }
 
   public ngOnInit(): void {
     this.userService.loadUsers();
@@ -27,4 +52,6 @@ export class UsersPageComponent {
   public onDeleteUser(userId: number): void {
     this.userService.deleteUser(userId)
   }
+
+
 }
